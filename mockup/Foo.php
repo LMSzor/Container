c954c4607e0a150f3dc2074818574fb0bebacb79
LMSzor <l.m.szor@gmail.com>
2017-12-03 19:48:07 +0100

    removed unnecessary files

--- mockup/Foo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace LMSzor\Mockup;
-
-class Foo {
-
-}
--- public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$c = new \LMSzor\Container\Container();
-
-$r = $c->get(\LMSzor\Mockup\Bar::class);
-
-var_dump($r);
